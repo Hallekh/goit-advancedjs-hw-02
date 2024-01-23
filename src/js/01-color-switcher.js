787b5cede0c8c1678fafb5e0b6ebf7245d9097e1
hallekh <lekhnovska.halyna@gmail.com>
2024-01-23 19:08:37 +0100

    task 1 color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const start = document.querySelector('.startBtn');
+const stop = document.querySelector('.stopBtn');
+const body = document.querySelector('body');
 
+start.addEventListener('click', changeColorHandler);
+stop.addEventListener('click', stopChangeColorHandler);
+let colorId = null;
+function changeColorHandler() {
+  stop.disabled = false;
+  colorId = setInterval(() => {
+    const currentColor = getRandomHexColor();
+    body.style.backgroundColor = currentColor;
+  }, 1000);
+  start.disabled = true;
+}
+function stopChangeColorHandler() {
+  start.disabled = false;
+  stop.disabled = true;
+  clearInterval(colorId);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
